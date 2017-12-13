5d9b9d2afbd3818374fbfa20a6f2f88ccf43c84a
Adrian Stabiszewski <as@grundid.de>
2017-12-13 16:49:32 +0100

    added momentjs

--- script.js
+++ script.js
@@ -1,76 +1,44 @@
-var openingYear = 2019;
-var openingMonth = 3;
-var openingDay = 1;
+const openingMoment = moment([2019, 3, 1]);
+const rotation = ["|", "/", "-", "\\"];
+let rotationIndex = 0;
 
-var openingDate = new Date(openingYear, (openingMonth - 1), openingDay);
-
-var countdownType;
-
-window.onload = function(){
-    updateTimePeriods();
-    countdownType = -1;
-    changeCountdown();
-
-    window.setInterval(function(){
-        updateTimePeriods();
-    }, 1);
-
-    window.setInterval(function(){
-        changeCountdown();
-    }, 2000);
+function updateCountdown(countdown) {
+    document.getElementById("countdown").innerHTML = countdown + "<br>(" + rotation[rotationIndex] + ")";
+    rotationIndex = (rotationIndex + 1) % rotation.length;
 }
 
-function updateTimePeriods()
-{
-    var now = new Date();
-
-    var pendingTime = openingDate - now;
-    //console.log(pendingTime);
-
-    var millisecondsShortenLength     =   1;
-    var secondLength                =   1000;
-    var minuteLength                =   secondLength  * 60;
-    var hourLength                  =   minuteLength  * 60;
-    var dayLength                   =   hourLength    * 24;
-
-    var pendingDays = Math.floor(pendingTime / dayLength);
-    //console.log(pendingDays);
-
-    var pendingHours = Math.floor(pendingTime / hourLength);
-    //Console.log(pendingHours);
-
-    var pendingMinutes = Math.floor(pendingTime / minuteLength);
-    //console.log(pendingMinutes);
-
-    var pendingSeconds = Math.floor(pendingTime / secondLength);
-    //console.log(pendingSeconds);
-
-    var pendingMillisecondsShorten = Math.floor(pendingTime / millisecondsShortenLength);
-    //console.log(pendingMillisecondsShorten);
-
-    updateHTML(pendingDays, pendingHours, pendingMinutes, pendingSeconds, pendingMillisecondsShorten);
+function showAllFields(duration) {
+    updateCountdown(duration.years() + " Jahr, " + duration.months() +
+        " Monaten, " + duration.days() + " Tagen, " + duration.hours() +
+        " Stunden, " + duration.minutes() + " Minuten und " + duration.seconds() + " Sekunden");
 }
 
-function updateHTML(pendingDays, pendingHours, pendingMinutes, pendingSeconds, pendingMillisecondsShorten){
-    document.getElementById("displayDays").innerHTML = pendingDays + " " + ((pendingDays > 1)? "Tagen" : "Tag");
-
-    document.getElementById("displayHours").innerHTML = pendingHours + " " + ((pendingHours > 1)? "Stunden" : "Stunde");
-
-    document.getElementById("displayMinutes").innerHTML = pendingMinutes + " " + ((pendingMinutes > 1)? "Minuten" : "Minute");
-
-    document.getElementById("displaySeconds").innerHTML = pendingSeconds + " " + ((pendingSeconds > 1)? "Sekunden" : "Sekunde");
-
-    document.getElementById("displayMillisecondsShorten").innerHTML = pendingMillisecondsShorten + " " + ((pendingMillisecondsShorten > 1)? "Millisekunden" : "Millisekunde");
+function showMillis(duration) {
+    updateCountdown(duration.as("milliseconds") + " Millisekunden");
 }
 
-function changeCountdown(){
-    countdownType++;
-    if(countdownType > 4){
-        countdownType = 0;
-    }
-    var allCountdowns = document.querySelectorAll(".timer");
-    for(var i = 0; i < allCountdowns.length; i++){
-        allCountdowns[i].style.display = 'none';
-    }
-    allCountdowns[countdownType].style.display = 'inline';
+function showDays(duration) {
+    updateCountdown((Math.round(duration.as("days") * 100) / 100) + " Tagen");
 }
+
+function showHours(duration) {
+    updateCountdown((Math.round(duration.as("hours") * 100) / 100) + " Stunden");
+}
+
+
+const countdowns = [showAllFields, showMillis, showDays, showHours];
+let currentCountdown = 0;
+
+window.onload = function () {
+
+
+    window.setInterval(function () {
+        const diff = openingMoment.diff(moment());
+        const duration = moment.duration(diff);
+        countdowns[currentCountdown](duration);
+    }, 85);
+
+    window.setInterval(function () {
+        currentCountdown = (currentCountdown + 1) % countdowns.length
+    }, 5000);
+};
